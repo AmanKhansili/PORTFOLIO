8f70c079ee64c6fe831d4f968c1c3936aa90d0bf
Aman Khansili <amankhansilee@gmail.com>
2024-02-05 22:18:01 +0530

    Adding scrollBtn and slider

--- src/component/hero/Hero.jsx
+++ src/component/hero/Hero.jsx
@@ -14,6 +14,27 @@ const textVariants = {
       staggerChildrem: 0.1,
     },
   },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 20,
+    },
+  },
 };
 
 const Hero = () => {
@@ -36,12 +57,22 @@ const Hero = () => {
             </motion.button>
             <motion.button variants={textVariants}>Contact Me</motion.button>
           </motion.div>
-          <motion.img variants={textVariants} src="/scroll.png" alt="" />
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png"
+            alt=""
+          />
         </motion.div>
       </div>
-      <div className="slidingTextCOntainer">
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
         Writer Content Creator Influencer
-      </div>
+      </motion.div>
       <div className="imageContainer">
         <img src="/hero.png" alt="" />
       </div>
