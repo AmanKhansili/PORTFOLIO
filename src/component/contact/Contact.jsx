e07db38b765664b8ac823186a30aff7e5f9100b3
Aman Khansili <amankhansilee@gmail.com>
2024-02-09 23:10:00 +0530

    contact

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -3,7 +3,20 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="contact">
-      <div className="textContainer"></div>
+      <div className="textContainer">
+        <h1>Let's work together</h1>
+        <div className="item">
+          <h2>Mail</h2>
+          <span>hello@gmail.com</span>
+        </div>
+        <div className="item">
+          <h2>Address</h2>
+          <span>Hello UK</span>
+        </div>
+        <div className="item">
+          <h2>Phone</h2><span>+1 655 4424</span>
+        </div>
+      </div>
       <div className="formContainer"></div>
     </div>
   );
