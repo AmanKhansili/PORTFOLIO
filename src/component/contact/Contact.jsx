41b26008539638ccf51e29d861b63f8a3d3cc3d4
Aman Khansili <amankhansilee@gmail.com>
2024-02-09 23:13:10 +0530

    Contact css start

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -14,10 +14,18 @@ const Contact = () => {
           <span>Hello UK</span>
         </div>
         <div className="item">
-          <h2>Phone</h2><span>+1 655 4424</span>
+          <h2>Phone</h2>
+          <span>+1 655 4424</span>
         </div>
       </div>
-      <div className="formContainer"></div>
+      <div className="formContainer">
+        <form>
+          <input type="text" required placeholder="Name" />
+          <input type="email" required placeholder="Email" />
+          <textarea rows={8} placeholder="Message" />
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
