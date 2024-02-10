25955f3016a60d0b25f934aff9809b446b48b877
Aman Khansili <amankhansilee@gmail.com>
2024-02-10 13:31:14 +0530

    Contact css

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -1,32 +1,57 @@
-import "./contact.scss"
+import "./contact.scss";
+import { motion } from "framer-motion";
+
+const variants = {
+  initial: {
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 const Contact = () => {
   return (
-    <div className="contact">
-      <div className="textContainer">
-        <h1>Let's work together</h1>
-        <div className="item">
+    <motion.div
+      className="contact"
+      variants={variants}
+      initial="initial"
+      whileInView="animate"
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <motion.h1 variants={variants}>Let's work together</motion.h1>
+        <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
           <span>hello@gmail.com</span>
-        </div>
-        <div className="item">
+        </motion.div>
+        <motion.div className="item" variants={variants}>
           <h2>Address</h2>
           <span>Hello UK</span>
-        </div>
-        <div className="item">
+        </motion.div>
+        <motion.div className="item" variants={variants}>
           <h2>Phone</h2>
           <span>+1 655 4424</span>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
       <div className="formContainer">
-        <form>
+        <motion.form
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ delay: 3, duration: 1 }}
+        >
           <input type="text" required placeholder="Name" />
           <input type="email" required placeholder="Email" />
           <textarea rows={8} placeholder="Message" />
           <button>Submit</button>
-        </form>
+        </motion.form>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
