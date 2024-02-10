908fbefc0feb848c15878d245216002088b237ee
Aman Khansili <amankhansilee@gmail.com>
2024-02-10 13:33:34 +0530

    formate

--- src/component/portfolio/Portfolio.jsx
+++ src/component/portfolio/Portfolio.jsx
@@ -39,13 +39,13 @@ const Single = ({ item }) => {
   const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
 
   return (
-    <section >
+    <section>
       <div className="container">
         <div className="wrapper">
           <div className="imageContainer" ref={ref}>
             <img src={item.img} alt="" />
           </div>
-          <motion.div className="textContainer" style={{y}}>
+          <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
             <button>See Demo</button>
