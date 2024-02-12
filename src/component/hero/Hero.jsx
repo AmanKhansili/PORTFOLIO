407d9415ef505e47beaf1b19871e1886d67660d6
Aman Khansili <amankhansilee@gmail.com>
2024-02-12 14:11:17 +0530

    Adding z-index

--- src/component/hero/Hero.jsx
+++ src/component/hero/Hero.jsx
@@ -53,9 +53,11 @@ const Hero = () => {
           </motion.h1>
           <motion.div className="buttons" variants={textVariants}>
             <motion.button variants={textVariants}>
-              See the Latest Works
+              <a href="#Portfolio">See the Latest Works</a>
             </motion.button>
-            <motion.button variants={textVariants}>Contact Me</motion.button>
+            <motion.button variants={textVariants}>
+              <a href="#Contact" type="button">Contact Me</a>
+            </motion.button>
           </motion.div>
           <motion.img
             variants={textVariants}
